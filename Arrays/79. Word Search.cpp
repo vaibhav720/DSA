c3fa63c231477fae8bd3fcc217b87783de5f34dc
vaibhav720 <parikhvaibhav19@gmail.com>
2022-05-07 22:17:00 +0530

    array

---
+++ Arrays/79. Word Search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        bool x=false;
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[i].size();j++)
+            {
+                int co=0;
+                if(word[co]==board[i][j])
+                {
+                    co++;
+                    int ii=i,jj=j;
+                    while(co<word.size())
+                    {
+                        if(ii+1<)
+                    }
+                }
+            }
+        }
+    }
+};
