4d94729ad01357006904da2b7439fc3d749197d6
vaibhav720 <parikhvaibhav19@gmail.com>
2022-05-23 21:20:47 +0530

    daily

---
+++ codeforces.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+//#pragma GCC optimize('O2') 
+#define ll long long 
+#define vll vector<ll>
+#define vi vector<int>
+#define tin tuple<ll,ll,ll>
+#define mll map<ll,ll>
+#define pi pair<ll,ll>
+#define vp vector<pi>
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define mt make_tuple
+#define all(x) (x).begin(),(x).end()
+#define rall(x) x.rbegin(), x.rend()
+#define watch(x) cout << (#x) << ' is ' << (x) << endl
+#define f(i,x,z) for(ll i=x;i<z;i++)
+#define rf(i,x,z) for(ll i=x-1;i>=z;i--)
+#define deb(x) cout<<'DEBUG------> '<<x<<endl
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define clr(x) memset(x, 0, sizeof(x)) 
+#define fa(i,vect) for(auto i : vect)
+#define fr(i,vect) for(VI :: reverse_iterator i = vect.rbegin(); i != vect.rend(); i++)
+#define s(a) (a).size()
+#define mod 1000000007
+// get<0>(v[i])
+// unordered_map<string, int> umap;
+// s.insert(0);multiset<int> ms;
+//auto it = s.lower_bound(a);
+//for (auto me : m) {
+//std::vector<ll>::iterator it; 
+//it = std::find (v.begin(), v.end(), ser);
+//v.erase(v.begin() +n);
+# define INF 0x3f3f3f3f 
+const int MAX = 1e3 + 7; 
+////////////////////////////////////////////////////////////////////////////////////////////
+
+ll power(ll base,ll por)
+{
+  ll res=1;
+   while(por>1){
+      if(por%2==1)
+      {
+         res*=base;
+         por--;
+      }
+      por/=2;
+      base*=base;
+   }
+   res*=base;
+   return res;
+}
+
+ll mo(ll a, ll b) {
+     ll c = a % b;
+     return (c < 0) ? c + b : c;
+  }
+
+
+long long gcd(long long  a, long long  b)
+{
+   if (b == 0)
+     return a;
+     return gcd(b, a % b);
+}
+
+long long lcm(ll a, ll b)
+{
+      return (a / gcd(a, b)) * b;
+}
+ ll dtob (ll a)
+{  ll r=1;
+   while((a&r)==0)r*=2;
+   return r;
+}
+
+
+
+
+bool isGoogles = 0;
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+//   freopen('a.txt', 'r', stdin); freopen('a_out.txt', 'w', stdout);
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t,n;
+    cin>>t;
+    f(h,0,t)
+    {
+       // if (isGoogles) {cout << 'Case #' << w << ': ';}
+        ll a,co=0,c=0,d=0,b=0,k;
+        cin>>n;
+        vector<int> v(n),v1(n),v2(n),v3(n),v4(n,0);
+        vector<pair<int,int>> ans;
+        f(i,0,n)
+        {
+            cin>>v[i];
+            v2[i]=v[i];
+        }
+        f(i,0,n)
+        {
+            cin>>v1[i];
+            v3[i]=v1[i];
+        }
+        sort(v2.begin(),v2.end());
+        sort(v3.begin(),v3.end());
+        bool x=false;
+        f(i,0,n)
+        {
+            f(j,0,n)
+            {
+                if(v2[i]==v[j] && v3[i]==v1[j] && v4[j]==0)
+                {
+                    v4[j]=i+1;
+                }
+            }
+        }       
+        for(int i=0;i<n;i++)
+        {
+            if(v4[i]==0)
+            {
+                x=true;
+                break;
+            }
+        }
+        if(x==true)
+        {
+            cout<<"-1\n";
+        }
+        else{
+            f(i,0,n)
+            {
+                while(i+1!=v4[i])
+                {
+                    ans.pb(mp(v4[v4[i]-1],v4[i]));
+                    swap(v4[v4[i]-1],v4[i]);
+                    co++;
+                }
+            }
+            cout<<co<<"\n";
+            f(i,0,ans.size())
+            {
+                cout<<ans[i].first<<" "<<ans[i].second<<"\n";
+            }
+        }
+        
+    }
+   return 0;
+}
